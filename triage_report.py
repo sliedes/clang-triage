1ae729cd8168a1d7bfca1b33a26e06e2b75617ce
Sami Liedes <sami.liedes@iki.fi>
2014-12-27 23:29:49 +0200

    Split triage_report.py main function.

--- triage_report.py
+++ triage_report.py
@@ -47,7 +47,6 @@ def get_num_distinct_reduced(db):
                   "FROM creduced_contents");
         return c.fetchone()[0]
 
-
 def get_num_reduce_failed(db):
     with db.cursor() as c:
         c.execute("SELECT COUNT(DISTINCT original) " +
@@ -63,7 +62,7 @@ def case_dict(sha, reduced=None):
             reduced[0], reduced[1], reduced)
     return d
 
-def failure_dict(sha, reason, old_reason, reduced=None):
+def build_failure_ctx(sha, reason, old_reason, reduced=None):
     d = case_dict(sha, reduced)
     d.update({'reason': reason, 'oldReason': old_reason})
     return d
@@ -97,6 +96,41 @@ def sort_cases(cases, reduced_dict, reduced_sizes):
 
     return unique_reduced + not_reduced + duplicate_reduced
 
+class TestRun:
+    def __init__(self, db, run_id, start_time, end_time, clang_ver, llvm_ver):
+        self.db = db
+        self.run_id = run_id
+        self.start_time = start_time
+        self.end_time = end_time
+        self.clang_ver = clang_ver
+        self.llvm_ver = llvm_ver
+        self.fails_dict, self.reduced_dict, self.reduced_sizes = fetch_failures(db, run_id)
+        self.version = 'clang {}, llvm {}'.format(clang_ver, llvm_ver)
+
+    def ctx(self, prev=None):
+        prev_fails = {}
+        prev_version = []
+        if prev:
+            prev_fails = prev.fails_dict
+            prev_version = prev.version
+        all_reasons = set(self.fails_dict.values())
+        all_reasons -= set(['OK'])
+        # changed failures
+        fails = [build_failure_ctx(x[0], x[1], prev_fails[x[0]],
+                              self.reduced_dict.get(x[0]))
+                 for x in sorted(self.fails_dict.items())
+                 if (x[0] in prev_fails
+                     and prev_fails[x[0]] != x[1])]
+        d = {'id': self.run_id, 'date': asctime(time.localtime(self.start_time)),
+             'duration': '{:d}'.format(self.end_time-self.start_time),
+             'version': self.version, 'prevVersion': prev_version,
+             'newFailures': fails,
+             'numDistinctFailures': len(all_reasons),
+             'endTime': asctime(time.localtime(self.end_time)),
+             'anyChanged?': len(fails)>0}
+        return d
+
+
 def main():
     with open('triage_report.pystache.xhtml') as f:
         TEMPLATE = pystache.parse(f.read())
@@ -108,36 +142,21 @@ def main():
                       'FROM test_runs ORDER BY start_time')
             res = c.fetchall()
         test_runs = []
-        old_fails = None
-        prev_version = None
-        for id_, start_time, end_time, clang_ver, llvm_ver in res:
-            fails_dict, reduced_dict, reduced_sizes = fetch_failures(db, id_)
-            all_reasons = set(fails_dict.values())
-            all_reasons -= set(['OK'])
-            # changed failures
-            fails = [failure_dict(x[0], x[1], old_fails[x[0]],
-                                  reduced_dict.get(x[0]))
-                     for x in sorted(fails_dict.items())
-                     if (not old_fails is None
-                         and x[0] in old_fails
-                         and old_fails[x[0]] != x[1])]
-            old_fails = fails_dict
-            version = 'clang {}, llvm {}'.format(clang_ver, llvm_ver)
-            d = {'id': id_, 'date': asctime(time.localtime(start_time)),
-                 'duration': '{:d}'.format(end_time-start_time),
-                 'version': version, 'prevVersion': prev_version,
-                 'newFailures': fails,
-                 'numDistinctFailures': len(all_reasons),
-                 'endTime': asctime(time.localtime(end_time)),
-                 'anyChanged?': len(fails)>0}
-            prev_version = version
-            test_runs.append(d)
+
+        prev_run = None
+        for run_id, start_time, end_time, clang_ver, llvm_ver in res:
+            test_run = TestRun(db, run_id, start_time, end_time,
+                               clang_ver, llvm_ver)
+            test_runs.append(test_run.ctx(prev_run))
+            prev_run = test_run
+        last_run = prev_run
 
         test_runs = test_runs[::-1]
         with db.cursor() as c:
             c.execute('SELECT COUNT(*) FROM case_contents')
             num_inputs = c.fetchone()[0]
 
+    assert test_runs, 'No test runs found.'
     num_runs_completed = len(test_runs)
     last_run_completed = test_runs[0]['endTime']
 
@@ -153,7 +172,7 @@ def main():
 
     # group failures by reason
     failures = {}
-    for sha, reason in fails_dict.items():
+    for sha, reason in last_run.fails_dict.items():
         if not reason in failures:
             failures[reason] = []
         failures[reason].append(sha)
@@ -162,13 +181,14 @@ def main():
         del failures['OK']
 
     # sort cases by size of reduced, secondarily by sha1
-    failures = [(x[0], sort_cases(x[1], reduced_dict, reduced_sizes))
+    failures = [(x[0], sort_cases(x[1], last_run.reduced_dict,
+                                  last_run.reduced_sizes))
                 for x in failures.items()]
     # sort by number of test cases triggering the crash
     failures.sort(key=lambda x: len(x[1]), reverse=True)
 
     failures = [{'reason': x[0],
-                 'cases': [case_dict(y, reduced_dict.get(y))
+                 'cases': [case_dict(y, last_run.reduced_dict.get(y))
                            for y in x[1][:MAX_SHOW_CASES]],
                  'numCases': len(x[1]),
                  'plural': len(x[1]) != 1,
